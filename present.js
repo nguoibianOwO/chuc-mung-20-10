93ee2ae5f7ee8a1a73c7962ac0625e07576f4bcc
nguoibianOwO <101120281+nguoibianOwO@users.noreply.github.com>
2022-10-19 13:48:03 +0700

    Update present.js

--- present.js
+++ present.js
@@ -1,5 +1,5 @@
 
-var to = 'có gì trong này';
+var to = 'mẹ iu của con <3';
 var gift_url = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/274731495_696944304655504_2468816002333192337_n.jpg?stp=dst-jpg_p180x540&_nc_cat=101&ccb=1-5&_nc_sid=aee45a&_nc_ohc=Momo-olp6m0AX-J_0nm&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVLQFB_84lW4XI706xp20YOuVy9_0fqx-on735U-nT8q3Q&oe=624B7EB3';
 var gift_image_url = 'mommy.jpg';
 
